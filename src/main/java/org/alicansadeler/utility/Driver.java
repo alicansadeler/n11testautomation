3af6c723b79000bcb4d11cd1e5918a366a8c361c
alicansadeler <alicansadeler@gmail.com>
2025-01-17 19:04:45 +0300

    chrome options eklendi

--- src/main/java/org/alicansadeler/utility/Driver.java
+++ src/main/java/org/alicansadeler/utility/Driver.java
@@ -2,8 +2,6 @@ package org.alicansadeler.utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.time.Duration;
 
 public class Driver {
@@ -11,19 +9,10 @@ public class Driver {
 
     public static WebDriver getDriver() {
         if (webDriver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--headless");
-
-            options.addArguments("--window-size=1920,1080");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--ignore-certificate-errors");
-
-            webDriver = new ChromeDriver(options);
+            webDriver = new ChromeDriver();
             webDriver.manage().window().maximize();
-            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-            webDriver.get(Constants.BASE_URL);
+
+            webDriver.get("https://www.n11.com/");
         }
         return webDriver;
     }
