047c2dd1e3a0c2d90b64288008522e25c300726f
alicansadeler <alicansadeler@gmail.com>
2025-01-17 20:07:41 +0300

    chrome options eklendi

--- src/main/java/org/alicansadeler/utility/Driver.java
+++ src/main/java/org/alicansadeler/utility/Driver.java
@@ -2,13 +2,18 @@ package org.alicansadeler.utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class Driver {
     private static WebDriver webDriver;
 
     public static WebDriver getDriver() {
         if (webDriver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
+
             webDriver = new ChromeDriver();
             webDriver.manage().window().maximize();
             webDriver.get(Constants.BASE_URL);
